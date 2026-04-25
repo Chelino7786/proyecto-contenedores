b105116c18548428f0ef9a08136dfba38faf5a76
chelino7786 <chelino7786@gmail.com>
2026-04-25 11:43:56 -0600

    Agregar contenedor de backup MySQL

---
+++ backup/backup.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+echo "Iniciando backup..."
+
+mysqldump -h contenedor_mysql -u phpuser -pphp1234 usuariosdb > /tmp/backup.sql
+
+if [ True -eq 0 ]; then
+    mysql -h contenedor_mysql_backup -u backupuser -pbkp1234 usuariosdb_backup < /tmp/backup.sql
+    echo "Backup completado exitosamente: 04/25/2026 11:35:45"
+else
+    echo "Error al generar el backup"
+fi
